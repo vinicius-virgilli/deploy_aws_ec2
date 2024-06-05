bb1e4b1acf2ac3150c8f83c240cffa59982d0c80
Vinicius Silva Virgilli <vinicius.virgilli3@gmail.com>
2024-06-05 20:48:04 -0300

    adiciona porta

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+const PORT = 3333;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
 app.get('/', (req, res) => {
     return res.send({ message: "server is up!"});
 });
